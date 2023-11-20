74730b0e39afd15540bc556c58fa9f12317e854f
Keerthana <keerthanaparsa23@gmail.com>
2023-11-20 08:21:15 -0800

    Adds Observability Changes

--- src/main/java/com/example/usersservice/UsersServiceApplication.java
+++ src/main/java/com/example/usersservice/UsersServiceApplication.java
@@ -3,7 +3,9 @@ package com.example.usersservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+//@EnableEurekaClient
 public class UsersServiceApplication {
 
 	public static void main(String[] args) {
